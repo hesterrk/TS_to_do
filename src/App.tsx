4606bdb70da8040a2aaf0949b6980c7149c39969
Hester Knight <hester2knight@mac.com>
2020-09-16 17:50:39 +0100

    basic set up of app: set out the type of the list state and the add function which will add a new task for the list, imported the form component, created basic form and limit functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ToDoForm from "../src/Components/Form";
 
 function App() {
+
+  // Type for array is an array of objects (tasks)
+  const myList: object[] = [];
+
+  const [toDoList, setToDoList] = useState(myList);
+
+
+  interface newTask {
+    task: String;
+    id: Number;
+    completed: Boolean;
+  };
+
+  
+
+  function addTask(taskName: newTask["task"]) {
+
+    const addTask = {
+      task: taskName,
+      id: Date.now(),
+      completed: false
+
+    }
+    
+    setToDoList([...toDoList, addTask]);
+  }
+
   return (
     <div className="App">
-      
+      <ToDoForm toDoList={toDoList} />
     </div>
   );
 }
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,3 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 
+interface Props {
+    toDoList: object[]
+}
 
+function ToDoForm({toDoList}: Props) {
+
+// State handling input change 
+  const [addTask, setAddTask] = useState('');
+
+    const onSubmit = (e: any) => {
+    setAddTask(addTask);
+
+    e.preventDefault();
+  };
+
+// The type of the event object is 'ChangeEvent' 
+    // Has a 'void' type of return as doesnt return anything
+  function onChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    setAddTask(e.target.value);
+  }
+
+  return (
+    <div>
+      <h3>Your Tasks:</h3>
+      
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={addTask}
+          onChange={onChange}
+          name="newTask"
+        />
+
+        <button type="submit">Add a Task </button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoForm;
